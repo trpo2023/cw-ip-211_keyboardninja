6088bb8ba246950076c41d860ff4932d4f02aeed
Flo4004 <flo6006@yandex.ru>
2023-04-06 18:39:18 +0700

    Move start_game func to game.h lib

--- source/main.c
+++ source/main.c
@@ -4,18 +4,15 @@
 #include <time.h>
 
 #include <lexer.h>
-
-#define TIME_FOR_GAME 30
+#include <game.h>
 
 int main()
 {
     srand(time(NULL));
     FILE *file;
     char **words = NULL;
-    int num_words = 0, level, score = 0, all_score = 0;
+    int num_words = 0, level;
     char *file_path;
-    time_t start_time = time(NULL);
-    char input_word[MAX_WORD_LENGTH], *output_word;
 
     printf("Choose difficulty:\n");
     printf("1. Easy\n");
@@ -52,26 +49,8 @@ int main()
 
     fclose(file);
 
-    score = 0;
-    all_score = 0;
-    while (time(NULL) - start_time < TIME_FOR_GAME)
-    {
-        output_word = words[rand() % num_words];
-        printf("> %s\n> ", output_word);
-        scanf("%s", input_word);
-        if (strcmp(input_word, output_word) == 0)
-        {
-            printf("Correct!\n");
-            score++;
-            all_score++;
-        }
-        else
-        {
-            printf("Incorrect!\n");
-            all_score++;
-        }
-    }
-    printf("You scored %d/%d. Congratulations!\n", score, all_score);
+    int score = start_game(words, num_words);
+    printf("%d\n", score);
 
     for (int i = 0; i < num_words; i++)
     {
